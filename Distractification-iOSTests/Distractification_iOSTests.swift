3f4b373b7ebab237fc05e2bec97f3d283da1673a
Andrew Harris <aharris86@gmail.com>
2016-05-20 15:46:10 -0400

    first ios push

---
+++ Distractification-iOSTests/Distractification_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Distractification_iOSTests.swift
+//  Distractification-iOSTests
+//
+//  Created by Andrew Harris on 5/20/16.
+//  Copyright © 2016 Andrew Harris. All rights reserved.
+//
+
+import XCTest
+@testable import Distractification_iOS
+
+class Distractification_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
